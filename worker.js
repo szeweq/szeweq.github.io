963ad91fd3cc98b3c72648b533da5df8b8dea4dd
Szewek <shevkpl@gmail.com>
2015-04-10 18:49:10 +0200

    Testing KAML parsing

--- worker.js
+++ worker.js
@@ -3,8 +3,7 @@ else postMessage(["using","xhr"]);
 
 var Handlers = {
 	fetchKAML:function(n){
-		fetch("style.css").then(function(r){
-			postMessage(["fetchKAML",r]);
+		fetch("badfile.x").then(function(r){
 		});
 	},
 	xhrKAML:function(n){
@@ -12,3 +11,32 @@ var Handlers = {
 	}
 }
 Handlers.fetchKAML();
+onmessage = function(m){
+	if (m.data[0] in Handlers) {
+		postMessage(["pending",m.data[0]]);
+		Handlers[m.data.shift()].apply(null,m.data);
+	}
+};
+
+function parseKAML(S,O){
+	var A=[];
+	return S
+	.replace(/<\?\s*([^>\s]*)\s*>/ig,function(f,m){return O[m]})
+	.replace(/<(\/|([^ >]*)([^>]*))>/ig,function(f,m,n,e){
+		if(n){
+			var I=[],C=[],T=[],
+				r=n.replace(/([.#])([^ .#]*)/g,function(xg,z,xs){(z=="."?C:z=="#"?I:[]).push(xs.replace("#",""));return "";}).replace(/^\/\//i,""),
+				v=n.match(/^\/\//i),
+				z=e.split(/ ([^="]=?"[^"]"|\/)/).filter(function(e){return e.length;}).map(function(e){
+					e=e.trim();
+					if(!e.indexOf("\"") && r in defaults) return defaults[r]+"="+e;
+					if(!e.indexOf("~")) return "style="+e.substring(1);
+					if(!e.indexOf("*")) return "data-"+e.substring(1);
+					return e;
+				}).join(" ");
+				h=r=="+";
+			if(!v&&!h)A.push(r);
+			return "<"+(h?"/"+A[A.length-1]+"><"+A[A.length-1]:r)+(I.length?" id=\""+I.join(" ")+"\"":"")+(C.length?" class=\""+C.join(" ")+"\"":"")+(z?" "+z:"")+(v?"/":"")+">";
+		} else if(m=="/")return "</"+A.pop()+">";
+	});
+}
