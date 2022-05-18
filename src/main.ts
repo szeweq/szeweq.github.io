0317005ce995920a0976b2211390b894ec232352
Szewek <shevkpl@gmail.com>
2022-05-18 20:29:30 +0200

    Remove pages completely

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+//import { createRouter, createWebHashHistory } from 'vue-router'
 //import { createPinia } from 'pinia'
-import routes from '~pages'
+//import routes from '~pages'
 //import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 
@@ -9,11 +9,11 @@ import './styles.css'
 
 const app = createApp(App)
 //const routes = setupLayouts(pages)
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+//const router = createRouter({
+//    history: createWebHashHistory(),
+//    routes
+//})
 //const pinia = createPinia()
-app.use(router)
+//app.use(router)
 //app.use(pinia)
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import generateSitemap from 'vite-plugin-pages-sitemap'
 import { VitePWA } from 'vite-plugin-pwa'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
@@ -18,11 +16,6 @@ export default defineConfig({
     Vue(),
     createHtmlPlugin({
       minify: true,
-    }),
-    Pages({
-      onRoutesGenerated(routes) {
-        generateSitemap({ routes, hostname: 'https://szeweq.github.io/' })
-      }
     }),
     Components({
       dts: 'src/components.d.ts',
